dbf1abbd8926d0da9edb63c055cc0b4ea84b61ce
Zipis Andrei <andrei.zipis8@gmail.com>
2024-11-07 04:46:43 -0800

    B

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_1/MainActivity.java
@@ -2,7 +2,6 @@ package ro.pub.cs.systems.eim.colocviu1_1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView labelText;
     private StringBuilder directions;
+    private int directionCount; // Numărul de puncte cardinale selectate
+
+    // Cheile pentru Bundle pentru a salva valorile
+    private static final String KEY_DIRECTION_COUNT = "direction_count";
+    private static final String KEY_DIRECTIONS = "directions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         directions = new StringBuilder();
+        directionCount = 0;
 
         labelText = findViewById(R.id.label_text);
 
-        // Butoanele punctelor cardinale
+        // Restaurare starea salvata, dacă există
+        if (savedInstanceState != null) {
+            directionCount = savedInstanceState.getInt(KEY_DIRECTION_COUNT, 0);
+            directions.append(savedInstanceState.getString(KEY_DIRECTIONS, ""));
+            updateLabelText();
+        }
+
+        // Butoanele pentru punctele cardinale
         Button buttonNorth = findViewById(R.id.button_north);
         Button buttonEast = findViewById(R.id.button_east);
         Button buttonSouth = findViewById(R.id.button_south);
@@ -43,9 +55,33 @@ public class MainActivity extends AppCompatActivity {
 
     private void appendDirection(String direction) {
         if (directions.length() > 0) {
-            directions.append(" -> "); // Delimitatorul dintre direcții
+            directions.append(" -> ");
         }
         directions.append(direction);
-        labelText.setText("Direcții apăsate: " + directions.toString());
+
+        // Incrementăm direcțiile selectate și actualizăm TextView
+        directionCount++;
+        updateLabelText();
+    }
+
+    private void updateLabelText() {
+        labelText.setText("Direcții apăsate: " + directions.toString() + " (" + directionCount + " apăsări)");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Salvăm valorile `directionCount` și `directions` în Bundle
+        outState.putInt(KEY_DIRECTION_COUNT, directionCount);
+        outState.putString(KEY_DIRECTIONS, directions.toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restaurăm valorile salvate în `directionCount` și `directions`
+        directionCount = savedInstanceState.getInt(KEY_DIRECTION_COUNT, 0);
+        directions = new StringBuilder(savedInstanceState.getString(KEY_DIRECTIONS, ""));
+        updateLabelText();
     }
 }
